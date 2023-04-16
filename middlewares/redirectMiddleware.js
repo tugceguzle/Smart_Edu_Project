0c50821d7c473b78b43dfe8a1a1b669ee2472b3f
Tugce Guzle <tugce.guzle1928@gmail.com>
2023-04-16 16:55:08 +0300

    update add student

--- middlewares/redirectMiddleware.js
+++ middlewares/redirectMiddleware.js
@@ -1,6 +1,6 @@
  module.exports = (req, res, next) => {
     if (req.session.userID) {
-      return res.redirect('/dashboard');
+      return res.redirect('user/dashboard');
     }
     next();
   };
